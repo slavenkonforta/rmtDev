9158c5e0b3c7e00fadeda3e8ee3992b3c9a604ce
Slaven Konforta <slaven.konforta@gmail.com>
2024-04-16 17:22:23 +0200

    memoisation

--- src/components/bookmarks-popover.tsx
+++ src/components/bookmarks-popover.tsx
@@ -9,8 +9,8 @@ const BookmarksPopover = forwardRef<HTMLDivElement>((_, ref) => {
   return createPortal(
     <div
       ref={ref}
-      className=' fixed left-1/2 top-[105px] z-10 flex min-h-[76px] w-[340px] min-w-[340px] origin-left -translate-x-1/2 items-center
-     justify-center overflow-hidden rounded-sm border bg-background shadow-lg dark:shadow-slate-900'
+      className='fixed left-1/2 top-[105px] z-10 flex min-h-[76px] w-[340px] min-w-[340px] origin-left -translate-x-1/2 items-center justify-center overflow-hidden
+     rounded-sm border bg-background shadow-lg transition-all dark:shadow-slate-900'
     >
       <JobList jobItems={bookmarkedJobItems} isLoading={isLoading} />
     </div>,
--- src/contexts/job-items-context-provider.tsx
+++ src/contexts/job-items-context-provider.tsx
@@ -2,7 +2,7 @@ import { useSearchQuery } from '@/hooks/use-search-query';
 import { useSearchTextContext } from '@/hooks/use-search-text-context';
 import { RESULTS_PER_PAGE } from '@/lib/constants';
 import { JobItem, PageDirection, SortBy } from '@/lib/types';
-import { createContext, useState } from 'react';
+import { createContext, useCallback, useMemo, useState } from 'react';
 
 type JobItemsContextProviderProps = {
   children: React.ReactNode;
@@ -31,47 +31,67 @@ export default function JobItemsContextProvider({ children }: JobItemsContextPro
   const totalNumberOfResults = jobItems?.length || 0;
   const totalNumberOfPages = Math.ceil(totalNumberOfResults / RESULTS_PER_PAGE);
 
-  const jobItemsSorted = [...(jobItems || [])].sort((a, b) => {
-    if (sortBy === 'recent') {
-      return a.daysAgo - b.daysAgo;
-    } else {
-      return b.relevanceScore - a.relevanceScore;
-    }
-  });
-
-  const jobItemsSortedAndSliced = jobItemsSorted.slice(
-    currentPage * RESULTS_PER_PAGE - RESULTS_PER_PAGE,
-    currentPage * RESULTS_PER_PAGE
+  const jobItemsSorted = useMemo(
+    () =>
+      [...(jobItems || [])].sort((a, b) => {
+        if (sortBy === 'recent') {
+          return a.daysAgo - b.daysAgo;
+        } else {
+          return b.relevanceScore - a.relevanceScore;
+        }
+      }),
+    [jobItems, sortBy]
   );
 
-  const handleSortByChange = (newSortBy: SortBy) => {
+  const jobItemsSortedAndSliced = useMemo(
+    () =>
+      jobItemsSorted.slice(
+        currentPage * RESULTS_PER_PAGE - RESULTS_PER_PAGE,
+        currentPage * RESULTS_PER_PAGE
+      ),
+    [jobItemsSorted, currentPage]
+  );
+
+  const handleSortByChange = useCallback((newSortBy: SortBy) => {
     setCurrentPage(1);
     setSortBy(newSortBy);
-  };
+  }, []);
 
-  const handlePageChange = (direction: PageDirection) => {
-    if (direction === 'next' && currentPage < totalNumberOfPages) {
-      setCurrentPage((prev) => prev + 1);
-    } else if (direction === 'previous' && currentPage > 1) {
-      setCurrentPage((prev) => prev - 1);
-    }
-  };
-
-  return (
-    <JobItemsContext.Provider
-      value={{
-        jobItems,
-        jobItemsSortedAndSliced,
-        totalNumberOfResults,
-        totalNumberOfPages,
-        isLoading,
-        sortBy,
-        currentPage,
-        handleSortByChange,
-        handlePageChange,
-      }}
-    >
-      {children}
-    </JobItemsContext.Provider>
+  const handlePageChange = useCallback(
+    (direction: PageDirection) => {
+      if (direction === 'next' && currentPage < totalNumberOfPages) {
+        setCurrentPage((prev) => prev + 1);
+      } else if (direction === 'previous' && currentPage > 1) {
+        setCurrentPage((prev) => prev - 1);
+      }
+    },
+    [currentPage, totalNumberOfPages]
   );
+
+  const contextValue = useMemo(
+    () => ({
+      jobItems,
+      jobItemsSortedAndSliced,
+      totalNumberOfResults,
+      totalNumberOfPages,
+      isLoading,
+      sortBy,
+      currentPage,
+      handleSortByChange,
+      handlePageChange,
+    }),
+    [
+      jobItems,
+      jobItemsSortedAndSliced,
+      totalNumberOfResults,
+      totalNumberOfPages,
+      isLoading,
+      sortBy,
+      currentPage,
+      handleSortByChange,
+      handlePageChange,
+    ]
+  );
+
+  return <JobItemsContext.Provider value={contextValue}>{children}</JobItemsContext.Provider>;
 }
