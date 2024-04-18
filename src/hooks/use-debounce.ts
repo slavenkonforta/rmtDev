2d4e946d972af5c5c4342abab26a65bf9f024516
Slaven Konforta <slaven.konforta@gmail.com>
2024-04-18 02:16:06 +0200

    jsdoc

--- src/hooks/use-debounce.ts
+++ src/hooks/use-debounce.ts
@@ -1,5 +1,12 @@
 import { useEffect, useState } from 'react';
 
+/**
+ * Debounces a value by delaying its update until a specified delay has passed.
+ * @template T - The type of the value being debounced.
+ * @param {T} value - The value to be debounced.
+ * @param {number} [delay=500] - The delay in milliseconds before updating the debounced value.
+ * @returns {T} - The debounced value.
+ */
 export const useDebounce = <T>(value: T, delay: number = 500) => {
   const [debouncedValue, setDebouncedValue] = useState(value);
 
--- src/hooks/use-on-click-outside.ts
+++ src/hooks/use-on-click-outside.ts
@@ -1,5 +1,10 @@
 import { useEffect } from 'react';
 
+/**
+ * Attaches an event listener to the document that triggers a handler function when a click event occurs outside of the specified element(s).
+ * @param refs - An array of React ref objects representing the element(s) to watch for clicks outside of.
+ * @param handler - The function to be called when a click event occurs outside of the specified element(s).
+ */
 export const useOnClickOutside = (refs: React.RefObject<HTMLElement>[], handler: () => void) => {
   useEffect(() => {
     const handleClick = (e: MouseEvent) => {
